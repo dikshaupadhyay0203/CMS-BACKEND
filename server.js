5bf16355957dd089ea8e43d607cdfd58d7d72e20
dikshaupadhyay0203 <dikshaupadhyay0203@gmail.com>
2026-02-12 16:16:04 +0530

    push webhook

--- server.js
+++ server.js
@@ -2,23 +2,28 @@ import dotenv from "dotenv";
 import "dotenv/config";
 dotenv.config();
 
+import http from "http";
+import { Server } from "socket.io";
+
 import app from "./app.js";
 import connectDB from "./config/db.js";
+import registerSocketHandlers from "./sockets/socket.js"; // check path
 
 const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-const server=Http2ServerRequest.createServer(app);
+const server = http.createServer(app);
+
 const io = new Server(server, {
     cors: {
         origin: "*",
-        Credentials: true,
+        credentials: true,
     }
 });
 
 registerSocketHandlers(io);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,26 +1,41 @@
-import { sendChatService } from "../services/chat.service";
+import { sendChatService } from "../services/chat.service.js";
 
-export const registerSocketHandlers = (io) => {
+ const registerSocketHandlers = (io) => {
 
     const onlineUsers = new Map();
 
     io.on("connection", (socket) => {
-        console.log("   User connected: ", socket.id);
+        console.log("User connected:", socket.id);
 
         socket.on("user-online", (userId) => {
             onlineUsers.set(userId, socket.id);
-            console.log("   User online: ", userId);
+            console.log("User online:", userId);
         });
 
         socket.on("send-message", async (data) => {
-            try{
+            try {
                 const { senderId, receiverId, message } = data;
-                const chat = await sendChatService({ senderId, receiverId, message });
+
+                const chat = await sendChatService({
+                    senderId,
+                    receiverId,
+                    message
+                });
+
                 const receiverSocketId = onlineUsers.get(receiverId);
+
                 if (receiverSocketId) {
                     io.to(receiverSocketId).emit("receive-message", chat);
-                }catch(error){
-                    console.error("Error sending message: ", error);
                 }
+
+            } catch (error) {
+                console.error("Error sending message:", error);
             }
-        }
+        });
+
+        socket.on("disconnect", () => {
+            console.log("User disconnected:", socket.id);
+        });
+    });
+};
+export default registerSocketHandlers;
